c8512c13dc84c6ddf1bb961198ae813d2ffae1d5
wuzhenyusjtu <wuzhenyusjtu@163.com>
2015-07-19 19:28:23 +0800

    All database operations are defined in this file

--- dbOperations.py
+++ dbOperations.py
@@ -19,12 +19,14 @@ def insertAPFeatures(bssid, ssid, channel, vendor, location, security, signal, n
 
 def insertAPCredits(bssid, location_history, useraccess_history, security_history, route_history, credit):
     db.insert('APsCredit', bssid=bssid, location_history=location_history, useraccess_history=useraccess_history, security_history=security_history, route_history=route_history, credit=credit)
-    
+'''    
 def insertUserAccess(ip, bssid):
     db.insert('UsersAccess',ip=ip,bssid=bssid,connectStart=datetime.datetime.utcnow())
-    
+'''
+'''
 def updateUserAccess(ip, bssid):
     db.update('UsersAccess', where='ip=$ip and bssid=$bssid',vars=locals(),connectEnd=datetime.datetime.utcnow())
+'''
 
 def insertRTTValue(bssid, meanProbe, meanDns, devProbe, devDns, rttEval):
     db.insert('RTTEvals', where='bssid=$bssid',vars=locals(),mean_probe=meanProbe,mean_dns=meanDns,dev_probe=devProbe,dev_dns=devDns,rtt_eval=rttEval)
